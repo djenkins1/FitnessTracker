7cdefeebdf00157f98408d1cd454e44ef4a24ffd
djenkins1 <djenkins1@email.arizona.edu>
2020-04-06 10:39:53 -0700

    Added test case for createWeek

--- src/test/java/com/djenkins/fitness/controller/TestFitnessWeekController.java
+++ src/test/java/com/djenkins/fitness/controller/TestFitnessWeekController.java
@@ -2,11 +2,12 @@ package com.djenkins.fitness.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,8 +21,10 @@ import java.util.List;
 import static org.hamcrest.Matchers.*;
 
 import com.djenkins.fitness.domain.FitnessWeek;
+import com.djenkins.fitness.factory.FitnessWeekBuilder;
 import com.djenkins.fitness.service.FitnessWeekService;
 import com.djenkins.fitness.util.FitnessWeekTestData;
+import com.djenkins.fitness.util.TestUtil;
 
 import static org.mockito.Mockito.*;
 
@@ -54,7 +57,9 @@ public class TestFitnessWeekController {
 		List<FitnessWeek> testDataResults = testData.getAllData();
 		when(fitnessWeekServiceMock.getAllFitnessWeek()).thenReturn(testDataResults);
 		mockMvc.perform(get(FitnessWeekEndpointConstants.GET_ALL)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$", hasSize(testDataResults.size()))).andExpect(jsonPath("$[0].id", is(1)));
+		// verify that the method was only called once
 		verify(fitnessWeekServiceMock, times(1)).getAllFitnessWeek();
 		verifyNoMoreInteractions(fitnessWeekServiceMock);
 	}
@@ -67,7 +72,9 @@ public class TestFitnessWeekController {
 
 		when(fitnessWeekServiceMock.getFitnessWeekById(id)).thenReturn(testResult);
 		final String urlEndpoint = FitnessWeekEndpointConstants.GET_WEEK.replaceFirst("\\{id\\}", "" + id);
-		mockMvc.perform(get(urlEndpoint)).andExpect(status().isOk()).andExpect(jsonPath("$.id", is(id.intValue())))
+		mockMvc.perform(get(urlEndpoint)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.id", is(id.intValue())))
 				.andExpect(jsonPath("$.totalTime", is(testResult.getTotalTime().intValue())))
 				.andExpect(jsonPath("$.totalMiles", is(testResult.getTotalMiles())))
 				.andExpect(jsonPath("$.totalCalories", is(testResult.getTotalCalories())))
@@ -76,17 +83,43 @@ public class TestFitnessWeekController {
 				.andExpect(jsonPath("$.dateRecorded", is(testResult.getDateRecorded().toString())))
 				.andExpect(jsonPath("$.exerciseType", is(testResult.getExerciseType())));
 
+		// verify that the method was only called once
 		verify(fitnessWeekServiceMock, times(1)).getFitnessWeekById(id);
 		verifyNoMoreInteractions(fitnessWeekServiceMock);
 	}
 
+	@Test
+	public void testCreateWeekSuccess() throws Exception {
+		List<FitnessWeek> testDataResults = testData.getAllData();
+		FitnessWeek testResult = testDataResults.get(0);
+		FitnessWeek testInput = new FitnessWeekBuilder(testResult).withId(null).build();
+
+		when(fitnessWeekServiceMock.createFitnessWeek(Mockito.any(FitnessWeek.class))).thenReturn(testResult);
+		mockMvc.perform(post(FitnessWeekEndpointConstants.CREATE_WEEK).contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(testInput))).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.id", is(testResult.getId().intValue())))
+				.andExpect(jsonPath("$.totalTime", is(testResult.getTotalTime().intValue())))
+				.andExpect(jsonPath("$.totalMiles", is(testResult.getTotalMiles())))
+				.andExpect(jsonPath("$.totalCalories", is(testResult.getTotalCalories())))
+				.andExpect(jsonPath("$.milesToDate", is(testResult.getMilesToDate().intValue())))
+				.andExpect(jsonPath("$.daysExercised", is(testResult.getDaysExercised())))
+				.andExpect(jsonPath("$.dateRecorded", is(testResult.getDateRecorded().toString())))
+				.andExpect(jsonPath("$.exerciseType", is(testResult.getExerciseType())));
+
+		// verify that the method was only called once
+		ArgumentCaptor<FitnessWeek> captor = ArgumentCaptor.forClass(FitnessWeek.class);
+		verify(fitnessWeekServiceMock, times(1)).createFitnessWeek(captor.capture());
+	}
+
+	// TODO: TEST CREATE WEEK WITH INVALID INPUTS
+
 	// TODO: TEST GET BETWEEN DATES
-	
+
 	// TODO: TEST GET BETWEEN SAME DATE
-	
+
 	// TODO: TEST GET BY EXERCISE TYPES
-	
+
 	// TODO: TEST GET BY MULTIPLE IDS
-	
-	// TODO: TEST CREATE WEEK
+
 }
