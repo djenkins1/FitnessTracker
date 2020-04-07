e473917b3732ab35f3596163cbfc9d9b103e44ca
djenkins1 <djenkins1@email.arizona.edu>
2020-04-07 11:01:59 -0700

    Added readme file and fixed tests for running in gradle

--- src/test/java/com/djenkins/fitness/FitnessApplicationTests.java
+++ src/test/java/com/djenkins/fitness/FitnessApplicationTests.java
@@ -25,26 +25,4 @@ class FitnessApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	void testInsertData() throws ParseException {
-		FitnessWeek fitnessWeek;
-		fitnessWeek = new FitnessWeek();
-		fitnessWeek.setDateRecorded(new Date(new SimpleDateFormat("M/d/yyyy").parse("2/28/2020").getTime()));
-		fitnessWeek.setExerciseType("Cycling");
-		fitnessWeek.setCreatedTs(Timestamp.from(Instant.now()));
-		fitnessWeek.setDaysExercised("W/T/F");
-		fitnessWeek.setMilesToDate(2622L);
-		fitnessWeek.setTotalCalories(284.0 + 999.9);
-		fitnessWeek.setTotalMiles(53.1);
-		fitnessWeek.setTotalTime(180L);
-		fitnessWeek = service.createFitnessWeek(fitnessWeek);
-		assertEquals(6, fitnessWeek.getId());
-	}
-	
-	@Test
-	void testDateParse() throws ParseException {
-		Date date = new Date(new SimpleDateFormat("M/d/yyyy").parse("2/28/2020").getTime());
-		assertEquals( "2020-02-28" , date.toString() );
-	}
 }
