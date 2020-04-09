5cfc12c04455bd899a26744f8931470692e9e265
djenkins1 <djenkins1@email.arizona.edu>
2020-04-09 09:10:32 -0700

    Fixed /graph giving error page

--- src/main/java/com/djenkins/fitness/controller/HomeController.java
+++ src/main/java/com/djenkins/fitness/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = { "/", "/graph" })
 	public String index() {
 		return "index";
 	}
