e84d16962ebd8b6c3d1fe8bae6e092628129496b
djenkins1 <djenkins1@email.arizona.edu>
2020-04-10 08:00:25 -0700

    Capture input on change and set state for Create week form

--- src/main/js/fitness-week-form.js
+++ src/main/js/fitness-week-form.js
@@ -6,16 +6,16 @@ class FitnessWeekForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			"formData": {
-				"totalTime": 0,
-				"totalMiles": 0,
-				"totalCalories": 0,
-				"milesToDate": 0,
-				"daysExercised": "",
-				"dateRecorded": "",
-				"exerciseType": ""
-			}
+			"totalTime": 0,
+			"totalMiles": 0,
+			"totalCalories": 0,
+			"milesToDate": 0,
+			"daysExercised": "",
+			"dateRecorded": "",
+			"exerciseType": ""
 		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
 	render() {
@@ -25,43 +25,43 @@ class FitnessWeekForm extends Component {
 				<Form.Field>
 					<Form.Label>Total Time(Minutes)</Form.Label>
 					<Form.Control>
-						<Form.Input type="number" placeholder="120" value={this.state.formData.totalTime} />
+						<Form.Input type="number" name="totalTime" placeholder="120" value={this.state.totalTime} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Total Miles</Form.Label>
 					<Form.Control>
-						<Form.Input type="number" placeholder="20.5" value={this.state.formData.totalMiles} />
+						<Form.Input type="number" name="totalMiles" placeholder="20.5" value={this.state.totalMiles} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Total Calories</Form.Label>
 					<Form.Control>
-						<Form.Input type="number" placeholder="250.25" value={this.state.formData.totalCalories} />
+						<Form.Input type="number" name="totalCalories" placeholder="250.25" value={this.state.totalCalories} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Miles to Date</Form.Label>
 					<Form.Control>
-						<Form.Input type="number" placeholder="1250" value={this.state.formData.milesToDate} />
+						<Form.Input type="number" name="milesToDate" placeholder="1250" value={this.state.milesToDate} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Days Exercised</Form.Label>
 					<Form.Control>
-						<Form.Input type="text" placeholder="M/T/W" value={this.state.formData.daysExercised} />
+						<Form.Input type="text" name="daysExercised" placeholder="M/T/W" value={this.state.daysExercised} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Exercise Type</Form.Label>
 					<Form.Control>
-						<Form.Input type="text" placeholder="Cycling" value={this.state.formData.exerciseType} />
+						<Form.Input type="text" name="exerciseType" placeholder="Cycling" value={this.state.exerciseType} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field>
 					<Form.Label>Date Recorded</Form.Label>
 					<Form.Control>
-						<Form.Input type="date" placeholder="2019-12-25" value={this.state.formData.dateRecorded} />
+						<Form.Input type="date" name="dateRecorded" placeholder="2019-12-25" value={this.state.dateRecorded} onChange={this.handleInputChange} />
 					</Form.Control>
 				</Form.Field>
 				<Form.Field kind="group">
@@ -74,6 +74,16 @@ class FitnessWeekForm extends Component {
 				</Form.Field>
 			</Box>
 		);
+	}
+
+	handleInputChange(event) {
+
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+		this.setState({
+			[name]: value
+		});
 	}
 }
 
