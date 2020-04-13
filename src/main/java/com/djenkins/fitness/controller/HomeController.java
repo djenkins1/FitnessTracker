16f3e233da94c645c84d612e8dc3ccb0eec66eae
djenkins1 <djenkins1@email.arizona.edu>
2020-04-13 13:19:20 -0700

    Added validation to form inputs for Create Week

--- src/main/java/com/djenkins/fitness/controller/HomeController.java
+++ src/main/java/com/djenkins/fitness/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = { "/", "/graph", "/create" })
+	@RequestMapping(value = { "/", "/graph", "/create", "/sumsAnnual" })
 	public String index() {
 		return "index";
 	}
--- src/main/js/FitnessWeekForm.js
+++ src/main/js/FitnessWeekForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Box, Heading, Form, Button } from 'react-bulma-components';
 import { Redirect } from 'react-router-dom';
+import FieldValidHelper from './FieldValidHelper';
+import Moment from 'moment';
 
 
 class FitnessWeekForm extends Component {
@@ -22,13 +24,18 @@ class FitnessWeekForm extends Component {
 			},
 			"dateRecorded": "",
 			"exerciseType": "Cycling",//default value
-			"redirect": false
+			"redirect": false,
+			"formValid": false,
+			"formErrors": {}
 		};
 
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleCancelClick = this.handleCancelClick.bind(this);
 		this.handleCalorieAddClick = this.handleCalorieAddClick.bind(this);
+		this.validateForm = this.validateForm.bind(this);
+		this.validateField = this.validateField.bind(this);
+		this.validateFieldAndChangeState = this.validateFieldAndChangeState.bind(this);
 	}
 
 	render() {
@@ -41,9 +48,9 @@ class FitnessWeekForm extends Component {
 		return (
 			<Box>
 				<Heading>{this.props.title}</Heading>
-				<form onSubmit={this.handleSubmit}>
+				<form onSubmit={this.handleSubmit} noValidate={true}>
 					<Form.Field>
-						<Form.Label>Total Time(Minutes)</Form.Label>
+						<Form.Label htmlFor="totalTime">Total Time(Minutes)</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
@@ -51,7 +58,10 @@ class FitnessWeekForm extends Component {
 						</Form.Control>
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Total Miles</Form.Label>
+						<FieldValidHelper errors={this.state.formErrors["totalTime"]} />
+					</Form.Field>
+					<Form.Field>
+						<Form.Label htmlFor="totalMiles">Total Miles</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
@@ -59,16 +69,22 @@ class FitnessWeekForm extends Component {
 						</Form.Control>
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Total Calories</Form.Label>
+						<FieldValidHelper errors={this.state.formErrors["totalMiles"]} />
+					</Form.Field>
+					<Form.Field>
+						<Form.Label htmlFor="totalCalories">Total Calories</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
 							<Form.Input type="number" step="0.1" min="0" name="totalCalories" placeholder="250.25" value={this.state.totalCalories} onChange={this.handleInputChange} />
 						</Form.Control>
-						<Button color="info" onClick={this.handleCalorieAddClick}>+999.9</Button>
+						<Button tabIndex="-1" color="info" onClick={this.handleCalorieAddClick}>+999.9</Button>
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Miles to Date</Form.Label>
+						<FieldValidHelper errors={this.state.formErrors["totalCalories"]} />
+					</Form.Field>
+					<Form.Field>
+						<Form.Label htmlFor="milesToDate">Miles to Date</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
@@ -76,19 +92,23 @@ class FitnessWeekForm extends Component {
 						</Form.Control>
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Days Exercised</Form.Label>
-						<Form.Control>
-							<Form.Checkbox name="monday" checked={this.state.daysExercised.monday} onChange={this.handleInputChange}>M</Form.Checkbox>
-							<Form.Checkbox name="tuesday" checked={this.state.daysExercised.tuesday} onChange={this.handleInputChange}>T</Form.Checkbox>
-							<Form.Checkbox name="wednesday" checked={this.state.daysExercised.wednesday} onChange={this.handleInputChange}>W</Form.Checkbox>
-							<Form.Checkbox name="thursday" checked={this.state.daysExercised.thursday} onChange={this.handleInputChange}>T</Form.Checkbox>
-							<Form.Checkbox name="friday" checked={this.state.daysExercised.friday} onChange={this.handleInputChange}>F</Form.Checkbox>
-							<Form.Checkbox name="saturday" checked={this.state.daysExercised.saturday} onChange={this.handleInputChange}>S</Form.Checkbox>
-							<Form.Checkbox name="sunday" checked={this.state.daysExercised.sunday} onChange={this.handleInputChange}>S</Form.Checkbox>
-						</Form.Control>
+						<FieldValidHelper errors={this.state.formErrors["milesToDate"]} />
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Exercise Type</Form.Label>
+						<Form.Label>Days Exercised</Form.Label>
+						<Form.Control>
+							<Form.Checkbox tabIndex="-1" name="monday" checked={this.state.daysExercised.monday} onChange={this.handleInputChange}>M</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="tuesday" checked={this.state.daysExercised.tuesday} onChange={this.handleInputChange}>T</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="wednesday" checked={this.state.daysExercised.wednesday} onChange={this.handleInputChange}>W</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="thursday" checked={this.state.daysExercised.thursday} onChange={this.handleInputChange}>T</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="friday" checked={this.state.daysExercised.friday} onChange={this.handleInputChange}>F</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="saturday" checked={this.state.daysExercised.saturday} onChange={this.handleInputChange}>S</Form.Checkbox>
+							<Form.Checkbox tabIndex="-1" name="sunday" checked={this.state.daysExercised.sunday} onChange={this.handleInputChange}>S</Form.Checkbox>
+						</Form.Control>
+						<FieldValidHelper errors={this.state.formErrors["daysExercised"]} />
+					</Form.Field>
+					<Form.Field>
+						<Form.Label htmlFor="exerciseType">Exercise Type</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
@@ -96,12 +116,18 @@ class FitnessWeekForm extends Component {
 						</Form.Control>
 					</Form.Field>
 					<Form.Field>
-						<Form.Label>Date Recorded</Form.Label>
+						<FieldValidHelper errors={this.state.formErrors["exerciseType"]} />
+					</Form.Field>
+					<Form.Field>
+						<Form.Label htmlFor="dateRecorded">Date Recorded</Form.Label>
 					</Form.Field>
 					<Form.Field kind="addons">
 						<Form.Control>
 							<Form.Input type="date" name="dateRecorded" placeholder="2019-12-25" value={this.state.dateRecorded} onChange={this.handleInputChange} />
 						</Form.Control>
+					</Form.Field>
+					<Form.Field>
+						<FieldValidHelper errors={this.state.formErrors["dateRecorded"]} />
 					</Form.Field>
 					<Form.Field kind="group">
 						<Form.Control>
@@ -127,27 +153,177 @@ class FitnessWeekForm extends Component {
 			this.setState(
 				{
 					"daysExercised": daysExercised
-				}
-			);
+				}, () => { this.validateFieldAndChangeState(name, target.checked) });
 		}
 		else {
 			this.setState({
 				[name]: value
-			});
+			}, () => { this.validateFieldAndChangeState(name, value) });
 		}
+	}
+
+	validateField(name, value) {
+		const formErrors = { ...this.state.formErrors }
+		var fieldTitle = "";
+		var formAttr = "";
+		if (name in this.state.daysExercised || name == "daysExercised") {
+			formAttr = "daysExercised";
+			fieldTitle = "Days Exercised";
+			const daysExercised = this.state.daysExercised;
+			const days = Object.keys(daysExercised);
+			const daysChecked = days.filter(function (day) {
+				return daysExercised[day]
+			});
+			if (daysChecked.length) {
+				formErrors[formAttr] = [];//set to empty list if valid
+			}
+			else {
+				//at least one day must be checked and none are checked currently
+				formErrors[formAttr] = [
+					fieldTitle + " must have at least one day selected."
+				];
+			}
+		}
+		else if (name === "totalTime" || name === "milesToDate") {
+			formAttr = name;
+			fieldTitle = (formAttr == "totalTime" ? "Total Time" : "Miles to Date");
+			if (value === '') {
+				//must not be empty
+				formErrors[formAttr] = [
+					fieldTitle + " must be a number."
+				];
+			}
+			else if (!isFinite(String(value)) || !(Math.floor(value) == value)) {
+				//must be numeric and integer
+				formErrors[formAttr] = [
+					fieldTitle + " must be a whole number(no decimal points)."
+				];
+			}
+			else if (parseInt(value) < 0) {
+				//must be greater than or equal to zero
+				formErrors[formAttr] = [
+					fieldTitle + " must be greater than or equal to zero."
+				];
+			}
+			else {
+				formErrors[formAttr] = [];//set to empty list if valid
+			}
+		}
+		else if (name === "totalCalories" || name === "totalMiles") {
+			fieldTitle = (name == "totalCalories" ? "Total Calories" : "Total Miles");
+			formAttr = name;
+			if (value === '') {
+				//must not be empty
+				formErrors[formAttr] = [
+					fieldTitle + " must be a number."
+				];
+			}
+			else if (!isFinite(String(value))) {
+				//must be number
+				formErrors[formAttr] = [
+					fieldTitle + " must be numeric."
+				];
+			}
+			else if (parseFloat(value) < 0) {
+				//must be greater than or equal to zero
+				formErrors[formAttr] = [
+					fieldTitle + " must be greater than or equal to zero."
+				];
+			}
+			else {
+				formErrors[formAttr] = [];//set to empty list if valid
+			}
+		}
+		else if (name === "dateRecorded") {
+			formAttr = name;
+			Moment.locale('en');//TODO: pull this out to some configuration?
+			fieldTitle = "Date Recorded";
+			const today = Moment();
+			const valueAsMomentDate = Moment(value, "YYYY-MM-DD");
+			if (!valueAsMomentDate.isValid()) {
+				//must be a recognizable date
+				formErrors[formAttr] = [
+					fieldTitle + " must be a date of the format 2020-12-31."
+				]
+			}
+			else if (valueAsMomentDate.isAfter(today)) {
+				//must not be in future
+				formErrors[formAttr] = [
+					fieldTitle + " must not be in the future."
+				]
+			}
+			else {
+				formErrors[formAttr] = [];//set to empty list if valid
+			}
+		}
+		else if (name === "exerciseType") {
+			formAttr = name;
+			fieldTitle = "Exercise Type";
+			formErrors[formAttr] = [];//set to empty list if valid
+			if (value.length == 0) {
+				//must not be empty
+				formErrors[formAttr] = [
+					fieldTitle + " must not be empty."
+				];
+			}
+			else {
+				formErrors[formAttr] = [];//set to empty list if valid
+			}
+		}
+		else {
+			console.log("Could not find validation for " + name + " with value " + value);
+		}
+
+		return formErrors[formAttr];
+	}
+
+	validateFieldAndChangeState(name, value) {
+		const fieldErrors = this.validateField(name, value);
+		const allErrors = { ...this.state.formErrors };
+		const formAttr = (name in this.state.daysExercised ? "daysExercised" : name);
+		allErrors[formAttr] = fieldErrors;
+		this.setState({
+			"formErrors": { ...allErrors }
+		});
+	}
+
+	validateForm() {
+		var isValid = true;
+		const formErrors = {};
+		formErrors["daysExercised"] = this.validateField("daysExercised", this.state.daysExercised);
+		formErrors["totalTime"] = this.validateField("totalTime", this.state.totalTime);
+		formErrors["totalMiles"] = this.validateField("totalMiles", this.state.totalMiles);
+		formErrors["totalCalories"] = this.validateField("totalCalories", this.state.totalCalories);
+		formErrors["milesToDate"] = this.validateField("milesToDate", this.state.milesToDate);
+		formErrors["dateRecorded"] = this.validateField("dateRecorded", this.state.dateRecorded);
+		formErrors["exerciseType"] = this.validateField("exerciseType", this.state.exerciseType);
+		for (var key in formErrors) {
+			if (formErrors[key] && formErrors[key].length > 0) {
+				isValid = false;
+				break;
+			}
+		}
+		this.setState({ "formValid": isValid, "formErrors": formErrors });
+		return isValid;
 	}
 
 	handleCalorieAddClick(event) {
 		event.preventDefault();
+		const oldCalories = (this.state.totalCalories != '' && isFinite(this.state.totalCalories) ? parseFloat(this.state.totalCalories) : 0);
+		const newCalories = 999.9 + oldCalories;
 		this.setState({
-			"totalCalories": parseFloat(this.state.totalCalories.toString()) + 999.9
-		});
+			"totalCalories": newCalories
+		}, () => { this.validateFieldAndChangeState("totalCalories", newCalories) });
 	}
-
 
 	handleSubmit(event) {
 		event.preventDefault();
-		this.createWeek(event, this.state);
+		if (this.validateForm()) {
+			this.createWeek(event, this.state);
+		}
+		else {
+			console.log("validateForm() returned false");
+		}
 	}
 
 	convertStateToFormData(stateData) {
