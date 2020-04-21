b6aae33f27ef5c41afe4ea09c5b416e7b29b740e
djenkins1 <djenkins1@email.arizona.edu>
2020-04-21 14:08:15 -0700

    Made last year comparison more readable

--- src/main/js/FitnessWeekCompareSumReport.js
+++ src/main/js/FitnessWeekCompareSumReport.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TimeUtil from './time-util';
 
 //show a this year to last year comparison given two values passed as props
 class FitnessWeekCompareSumReport extends Component {
@@ -6,9 +7,10 @@ class FitnessWeekCompareSumReport extends Component {
 		if (this.props.thisYearValue && this.props.lastYearValue) {
 			const sumDiff = this.props.thisYearValue - this.props.lastYearValue;
 			const arrow = (sumDiff >= 0 ? (<i className="fas fa-arrow-up has-text-success" />) : (<i className="fas fa-arrow-down has-text-danger" />))
+			const displayDiff = this.displaySumDiffBasedOnType(sumDiff, this.props.displayType);
 			return (
 				<td>
-					{arrow} {Math.abs(sumDiff)} compared to last year.
+					{arrow} {displayDiff} compared to last year.
 				</td>
 			);
 		}
@@ -18,6 +20,20 @@ class FitnessWeekCompareSumReport extends Component {
 			);
 		}
 
+	}
+
+	displaySumDiffBasedOnType(diffValue, displayType) {
+		const positiveDiffValue = Math.abs(diffValue);
+		if (displayType === "time") {
+			return TimeUtil.convertMinutes(positiveDiffValue);
+		}
+		else if (displayType === "decimal") {
+			return positiveDiffValue.toFixed(1);
+		}
+		else {
+			console.log("Could not translate display type for CompareSumReport: " + displayType);
+			return positiveDiffValue;
+		}
 	}
 }
 
--- src/main/js/FitnessWeekSumReport.js
+++ src/main/js/FitnessWeekSumReport.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Container, Box, Notification, Heading, Table } from 'react-bulma-components';
 import FitnessWeekCompareSumReport from './FitnessWeekCompareSumReport';
+import TimeUtil from './time-util';
 
 class FitnessWeekSumReport extends Component {
 	render() {
 		if (this.props.sumData && this.props.sumData.totalCalories !== undefined) {
-			const timeInDaysHoursMinutes = this.convertMinutes(this.props.sumData.totalTime);
+			const timeInDaysHoursMinutes = TimeUtil.convertMinutes(this.props.sumData.totalTime);
 			return (
 				<Box>
 					<Container>
@@ -16,19 +17,19 @@ class FitnessWeekSumReport extends Component {
 									<td><b>Total Time</b></td>
 									<td>{timeInDaysHoursMinutes}</td>
 									<FitnessWeekCompareSumReport thisYearValue={this.props.sumData.totalTime}
-										lastYearValue={this.props.sumDataLastYear.totalTime} />
+										lastYearValue={this.props.sumDataLastYear.totalTime} displayType="time" />
 								</tr>
 								<tr>
 									<td><b>Total Calories</b></td>
 									<td>{this.props.sumData.totalCalories.toLocaleString()} Calories</td>
 									<FitnessWeekCompareSumReport thisYearValue={this.props.sumData.totalCalories}
-										lastYearValue={this.props.sumDataLastYear.totalCalories} />
+										lastYearValue={this.props.sumDataLastYear.totalCalories} displayType="decimal" />
 								</tr>
 								<tr>
 									<td><b>Total Miles</b></td>
 									<td>{this.props.sumData.totalMiles.toLocaleString()} Miles</td>
 									<FitnessWeekCompareSumReport thisYearValue={this.props.sumData.totalMiles}
-										lastYearValue={this.props.sumDataLastYear.totalMiles} />
+										lastYearValue={this.props.sumDataLastYear.totalMiles} displayType="decimal" />
 								</tr>
 							</tbody>
 						</Table>
@@ -50,21 +51,6 @@ class FitnessWeekSumReport extends Component {
 				</Box>
 			);
 		}
-	}
-
-	convertMinutes(totalMinutes) {
-		//minutes to hour (and days) converter
-		const MINUTES_PER_HOUR = 60;
-		const HOURS_PER_DAY = 24;
-		const MINUTES_PER_DAY = MINUTES_PER_HOUR * HOURS_PER_DAY;
-		let d = Math.floor(totalMinutes / MINUTES_PER_DAY);
-		let h = Math.floor((totalMinutes - (d * MINUTES_PER_DAY)) / MINUTES_PER_HOUR);
-		let m = Math.round(totalMinutes % MINUTES_PER_HOUR);
-		let dayUnitStr = (d == 1 ? " Day " : " Days ");
-		let hourUnitStr = (h == 1 ? " Hour " : " Hours ");
-		let minUnitStr = (m == 1 ? " Minute" : " Minutes");
-
-		return (d + dayUnitStr + h + hourUnitStr + m + minUnitStr);
 	}
 }
 
